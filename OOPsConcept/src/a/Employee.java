140f9c2fdfdd56908fee234e0e746c6e47c28ce5
Panos <pidkaris@gmail.com>
2023-06-15 09:14:37 +0000

    Java program v2

--- OOPsConcept/src/a/Employee.java
+++ OOPsConcept/src/a/Employee.java
@@ -1,10 +1,19 @@
 package a;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Employee {
 	
 	int id;
 	String name;
 	float salary;			// property or instance variable
+	
+	Employee(){
+		System.out.println("Object created...");
+		id=123;
+		name="Unkown";
+		salary= 3;
+	}
 	
 	void setValue(int id1, String name1,float salary1) {
 		id = id1;
--- OOPsConcept/src/a/EmployeeTest.java
+++ OOPsConcept/src/a/EmployeeTest.java
@@ -4,20 +4,22 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee emp1 = new Employee();
-		Employee emp2 = new Employee();
-		Employee emp3 = new Employee();
-		emp1.name = "Joe";
-		emp1.salary = 120000;
-		emp1.id = 12;
-		emp2.name = "Steven";
-		emp2.salary = 180000;
-		emp2.id = 13;
-		emp3.setValue(14, "Luke",18000);
-		
-		emp1.display();
-		emp2.display();
-		emp3.display();
+//		Employee emp1 = new Employee();
+//		Employee emp2 = new Employee();
+//		Employee emp3 = new Employee();
+//		emp1.name = "Joe";
+//		emp1.salary = 120000;
+//		emp1.id = 12;
+//		emp2.name = "Steven";
+//		emp2.salary = 180000;
+//		emp2.id = 13;
+//		emp3.setValue(14, "Luke",18000);
+//		
+//		emp1.display();
+//		emp2.display();
+//		emp3.display();
+		Employee emp4 = new Employee();
+		emp4.display();
 		
 
 	}
