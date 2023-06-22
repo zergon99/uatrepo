2780d87325875e131aa4d1a1e9c88b1c916e7c33
Panos <pidkaris@gmail.com>
2023-06-22 08:38:37 +0000

    Java program v2

---
+++ PolymorExample/src/com/MethodOverloadingExample.java
@@ -0,0 +1,19 @@
+package com;
+
+public class MethodOverloadingExample {
+
+	public static void main(String[] args) {
+		
+		Operation op = new Operation();
+		op.add(10);
+		op.add(10.10, 20.20);
+		op.add(10,20);
+		op.add("10","20");
+	}
+
+
+		
+		
+	}
+
+
---
+++ PolymorExample/src/com/Operation.java
@@ -0,0 +1,25 @@
+package com;
+
+
+	public class Operation {
+		void add() {
+			System.out.println(10+20);
+		}
+		void add(int x) {
+			System.out.println(x+100);
+		}
+		void add(int x, int y) {
+			System.out.println(x+y);
+		}
+		void add(int x, int y, int z) {
+			System.out.println(x+y+z);
+		}
+		void add(double x, double y) {
+			System.out.println(x+y);
+		}
+		void add(String x, String y) {
+			System.out.println(x+y);
+		}
+	}
+
+
